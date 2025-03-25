fd77fe9df1d4f4512dc6c57c5a848b288f98f021
hyoni-ch <asdfgh9933@daum.net>
2025-03-25 22:03:50 +0900

    footer 수정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,44 @@
 import Link from "next/link";
 import React from "react";
 
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import XIcon from "@mui/icons-material/X";
+
 function Footer() {
   const snsData = [
     {
       id: 1,
-      name: "instargram",
+      name: "instagram",
+      icon: InstagramIcon,
       path: "https://www.instagram.com/joyuri_offcl/#",
     },
-    { id: 2, name: "facebook", path: "https://www.facebook.com/JOYURI.offcl" },
-    { id: 3, name: "x", path: "https://x.com/JOYURI_offcl" },
+    {
+      id: 2,
+      name: "facebook",
+      icon: FacebookIcon,
+      path: "https://www.facebook.com/JOYURI.offcl",
+    },
+    { id: 3, name: "x", icon: XIcon, path: "https://x.com/JOYURI_offcl" },
     {
       id: 4,
       name: "spotify",
+      icon: YouTubeIcon,
       path: "https://open.spotify.com/artist/3LFFf4EpKn2krneZ9vozyz",
     },
     {
       id: 5,
       name: "youtube",
+      icon: YouTubeIcon,
       path: "https://www.youtube.com/channel/UCSEVFgCkKem_c3nIBm7F37g",
     },
-    { id: 6, name: "tiktok", path: "https://www.tiktok.com/@joyuri.offcl" },
+    {
+      id: 6,
+      name: "tiktok",
+      icon: YouTubeIcon,
+      path: "https://www.tiktok.com/@joyuri.offcl",
+    },
   ];
 
   return (
@@ -36,13 +54,15 @@ function Footer() {
         {snsData.map((sns) => {
           return (
             <li key={sns.id}>
-              <Link href={sns.path}>{sns.name}</Link>
+              <Link href={sns.path}>
+                <sns.icon fontSize="large" />
+              </Link>
             </li>
           );
         })}
       </ul>
 
-      <p style={{ textAlign: "center", marginTop: "1rem" }}>
+      <p style={{ textAlign: "center", marginTop: "1rem", color: "#7d7d7d" }}>
         COPYRIGHTⓒ 2025 PROJECT. ALL RIGHTS RESERVED.
       </p>
     </div>
@@ -53,6 +73,5 @@ export default Footer;
 
 const footerStyle = {
   backgroundColor: "rgba(0, 0, 0, 0.1)",
-  color: "#7d7d7d",
   padding: "2rem",
 };
