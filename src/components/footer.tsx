0383bbe155b49dbf42eebc95a01f9ac1b93b973b
hyoni-ch <asdfgh9933@daum.net>
2025-03-25 20:21:38 +0900

    footer 수정 및 style 설정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,29 +2,57 @@ import Link from "next/link";
 import React from "react";
 
 function Footer() {
-  return (
-    <div>
-      <div style={footerLinkStyle}>
-        <Link href="https://www.facebook.com/JOYURI.offcl">facebook</Link>
-        <Link href="https://www.instagram.com/joyuri_offcl/#">instagram</Link>
-        <Link href="https://x.com/JOYURI_offcl">x</Link>
-        <Link href="https://open.spotify.com/artist/3LFFf4EpKn2krneZ9vozyz">
-          spotify
-        </Link>
-        <Link href="https://www.youtube.com/channel/UCSEVFgCkKem_c3nIBm7F37g">
-          youtube
-        </Link>
-        <Link href="https://www.tiktok.com/@joyuri.offcl">tiktok</Link>
-      </div>
+  const snsData = [
+    {
+      id: 1,
+      name: "instargram",
+      path: "https://www.instagram.com/joyuri_offcl/#",
+    },
+    { id: 2, name: "facebook", path: "https://www.facebook.com/JOYURI.offcl" },
+    { id: 3, name: "x", path: "https://x.com/JOYURI_offcl" },
+    {
+      id: 4,
+      name: "spotify",
+      path: "https://open.spotify.com/artist/3LFFf4EpKn2krneZ9vozyz",
+    },
+    {
+      id: 5,
+      name: "youtube",
+      path: "https://www.youtube.com/channel/UCSEVFgCkKem_c3nIBm7F37g",
+    },
+    { id: 6, name: "tiktok", path: "https://www.tiktok.com/@joyuri.offcl" },
+  ];
 
-      <p>COPYRIGHTⓒ 2025 PROJECT. ALL RIGHTS RESERVED.</p>
+  return (
+    <div style={footerStyle}>
+      <ul
+        style={{
+          listStyleType: "none",
+          display: "flex",
+          gap: 10,
+          justifyContent: "center",
+        }}
+      >
+        {snsData.map((sns) => {
+          return (
+            <li key={sns.id}>
+              <Link href={sns.path}>{sns.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <p style={{ textAlign: "center", marginTop: "1rem" }}>
+        COPYRIGHTⓒ 2025 PROJECT. ALL RIGHTS RESERVED.
+      </p>
     </div>
   );
 }
 
 export default Footer;
 
-const footerLinkStyle = {
-  display: "flex",
-  gap: 10,
+const footerStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.1)",
+  color: "#7d7d7d",
+  padding: "2rem",
 };
