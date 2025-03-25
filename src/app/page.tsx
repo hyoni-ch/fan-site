6d7aae12ec088136baae012706803d8307cbd66e
zoeyourlife <201814112@office.skhu.ac.kr>
2025-03-25 14:21:00 +0900

    env: mui 설치 및 theme 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import api from "@/utils/api";
-import { logout } from "@/utils/auth";
+// import { logout } from "@/utils/auth";
+import { Box, Container, Typography } from "@mui/material";
+import Link from "@mui/material/Link";
+import NextLink from "next/link";
 
 function Home() {
   api.get("/home").then((response) => {
@@ -9,9 +12,27 @@ function Home() {
   });
 
   return (
-    <button type="button" onClick={logout}>
-      로그아웃
-    </button>
+    // <button type="button" onClick={logout}>
+    //   로그아웃
+    // </button>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI - Next.js App Router example in TypeScript
+        </Typography>
+        <Link href="/about" color="secondary" component={NextLink}>
+          Go to the about page
+        </Link>
+      </Box>
+    </Container>
   );
 }
 
