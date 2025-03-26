89c047ff78356133f25eea2840ee2577cd75e131
hyoni-ch <asdfgh9933@daum.net>
2025-03-27 01:23:34 +0900

    메인페이지 섹션 스크롤 이동 기능 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,76 @@
 "use client";
 
 // import { logout } from "@/utils/auth";
-import { Box, Container, Typography } from "@mui/material";
-import Link from "@mui/material/Link";
-import NextLink from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [currentSection, setCurrentSection] = useState<number>(0);
+  const [sectionHeight, setSectionHeight] = useState<number>(0);
+  const sections = [0, 1, 2, 3];
+
+  useEffect(() => {
+    setSectionHeight(window.innerHeight);
+  }, []);
+
+  const handleScroll = (e: WheelEvent) => {
+    if (e.deltaY > 0) {
+      if (currentSection < sections.length - 1) {
+        setCurrentSection((prev) => prev + 1);
+      }
+    } else {
+      if (currentSection > 0) {
+        setCurrentSection((prev) => prev - 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleScroll);
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [currentSection]);
+
+  useEffect(() => {
+    if (sectionHeight > 0) {
+      window.scrollTo({
+        top: currentSection * sectionHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [currentSection, sectionHeight]);
+
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+    <div>
+      <section style={{ position: "relative", width: "100%", height: "100vh" }}>
+        <Image
+          src="/images/mainImg.jpg"
+          alt="mainImg"
+          layout="fill"
+          objectFit="cover"
+          style={{ position: "absolute", top: 0, left: 0 }}
+        />
+      </section>
+
+      <section
+        style={{
+          height: "100vh",
+          backgroundColor: "#aaa",
         }}
       >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary" component={NextLink}>
-          Go to the about page
-        </Link>
-      </Box>
-    </Container>
+        <h1>DISCOGRAPHY</h1>
+      </section>
+
+      <section
+        style={{
+          height: "100vh",
+          backgroundColor: "#bbb",
+        }}
+      >
+        <h1>GOODS</h1>
+      </section>
+    </div>
   );
 }
 
