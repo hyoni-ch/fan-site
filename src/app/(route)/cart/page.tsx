2f668266fb7450095881ced56425bf8d4a89f9ad
zoeyourlife <201814112@office.skhu.ac.kr>
2025-03-26 18:45:35 +0900

    feat: 전역 인증 상태 확인 검사 cart 페이지
    추후에 mypage에도 유효성 검사 들어가야함

--- src/app/(route)/cart/page.tsx
+++ src/app/(route)/cart/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
 
-function page() {
-  return <div>page</div>;
+import { logout } from "@/api/auth";
+import useAuthStore from "@/store/authStore";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+function Page() {
+  const router = useRouter();
+  const { accessToken } = useAuthStore();
+  const [isAuthChecked, setIsAuthChecked] = useState(false);
+
+  useEffect(() => {
+    setIsAuthChecked(true);
+  }, []);
+
+  useEffect(() => {
+    if (isAuthChecked && !accessToken) {
+      router.push("/login");
+    }
+  }, [isAuthChecked, accessToken, router]);
+
+  if (!isAuthChecked) {
+    return null;
+  }
+
+  const handleLogout = () => {
+    logout();
+    router.push("/");
+  };
+  return (
+    <div>
+      <h1>메롱</h1>
+      <button onClick={handleLogout}>로그아웃</button>
+    </div>
+  );
 }
 
-export default page;
+export default Page;
