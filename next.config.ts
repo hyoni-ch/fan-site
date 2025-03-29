b511b5d63ef377679269d4b9525963a607f72b5d
hyoni-ch <asdfgh9933@daum.net>
2025-03-30 02:39:02 +0900

    외부 도메인 추가 (imgaes)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["61.99.26.112"],
+  },
 };
 
 export default nextConfig;
