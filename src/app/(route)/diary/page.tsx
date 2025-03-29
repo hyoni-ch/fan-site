f69c1265a6869bc528b37b02d25e6f563ce6a09a
zoeyourlife <201814112@office.skhu.ac.kr>
2025-03-30 00:35:27 +0900

    feat: diary 메인 페이지 컴포넌트 UI 구성 및 모듈화
    추후 유지보수 생각해서 메인페이지 UI는 모듈화

---
+++ src/app/(route)/diary/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Box } from "@mui/material";
+import DiaryImage from "@/components/diaryPage/diaryImage";
+import { useEffect, useState } from "react";
+import getLatestDiaryEntries, { FourArticle } from "@/api/getLatestDiary";
+import { API_BASED_URL } from "@/constants/apiUrl";
+
+function Diary() {
+  const [diaryImages, setDiaryImages] = useState<FourArticle[]>([]);
+
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
+    0
+  );
+  const [hoveredImageIndex, setHoveredImageIndex] = useState<number | null>(
+    null
+  );
+
+  useEffect(() => {
+    async function fetchDiaryEntries() {
+      const entries = await getLatestDiaryEntries();
+      setDiaryImages(entries);
+    }
+    fetchDiaryEntries();
+  }, []);
+
+  const handleImageClick = (index: number) => {
+    setSelectedImageIndex(index);
+  };
+
+  const handleHover = (index: number) => {
+    setHoveredImageIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredImageIndex(null);
+  };
+
+  return (
+    <Box sx={{ display: "flex", width: "100%", height: "880px" }}>
+      {diaryImages.map((image, index) => (
+        <DiaryImage
+          key={image.id}
+          id={image.id}
+          src={
+            `${API_BASED_URL}` + image.articleImageList[0]?.url ||
+            "/images/diary1.png"
+          }
+          alt={image.title}
+          diaryTitle={image.title}
+          createDate={image.createDate}
+          selected={selectedImageIndex}
+          index={index}
+          onClick={handleImageClick}
+          onHover={handleHover}
+          onMouseLeave={handleMouseLeave}
+          hoveredImageIndex={hoveredImageIndex}
+        />
+      ))}
+    </Box>
+  );
+}
+
+export default Diary;
