cca484990f5d06313bc13e71534db6ef2fe04e6e
zoeyourlife <201814112@office.skhu.ac.kr>
2025-03-31 10:58:47 +0900

    feat: 프로필 페이지에서 사용될 카테고리별 인터페이스 정의
    인터페이스 정의

---
+++ src/types/iprofile.ts
@@ -0,0 +1,37 @@
+//앨범 인터페이스 정의
+export interface Album {
+  id: number;
+  title: string;
+  description: string;
+  releaseDate: string;
+  albumImages: AlbumImage[];
+}
+export interface AlbumImage {
+  id: number;
+  url: string;
+}
+// 드라마 인터페이스 정의
+export interface DramaList {
+  id: number;
+  careerName: string;
+  description: string;
+  url: string;
+  releaseDate: number;
+  careerImages: DramaImage[];
+}
+export interface DramaImage {
+  id: number;
+  url: string;
+}
+//콘서트 인터페이스 정의
+export interface ConcertList {
+  id: number;
+  title: string;
+  year: number;
+  concertImages: ConcertImage[];
+  // imageUrl: string;
+}
+export interface ConcertImage {
+  id: number;
+  url: string;
+}
