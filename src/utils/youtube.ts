d195e61307d2afdf2839d21d72c416ac9cae639d
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-02 01:06:13 +0900

    feat: youtube link formatting
    유튜브 영상 링크 포맷팅

---
+++ src/utils/youtube.ts
@@ -0,0 +1,15 @@
+export function getYouTubeVideoId(url: string | undefined) {
+  if (!url) return null;
+  const regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[7].length === 11 ? match[7] : null;
+}
+
+export function createYouTubeIframe(
+  videoId: string,
+  width = "1152px",
+  height = "600px"
+) {
+  return `<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+}
