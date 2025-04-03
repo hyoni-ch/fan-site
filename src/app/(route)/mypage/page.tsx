d391d0bdf56e1f34d4363aabb8ed116316ea2e2e
hyoni-ch <asdfgh9933@daum.net>
2025-04-04 02:57:15 +0900

    마이페이지 로그아웃 요청 수정

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from "react";
 import api from "@/utils/api";
 import useAuthStore from "@/store/authStore";
 import { useRouter } from "next/navigation";
-import { Box, Button, TextField, Typography } from "@mui/material";
-import { getUserInfo } from "@/api/auth";
+import { Box, Button, Tab, Tabs, TextField, Typography } from "@mui/material";
+import { getUserInfo, logout } from "@/api/auth";
 
 interface UserInfo {
   username: string;
@@ -13,7 +13,7 @@ interface UserInfo {
 }
 
 function Mypage() {
-  const [activeMenu, setActiveMenu] = useState("profile");
+  const [activeTab, setActiveTab] = useState("profile");
   const [oldPassword, setOldPassword] = useState<string>("");
   const [newPassword, setNewPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -21,7 +21,6 @@ function Mypage() {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const router = useRouter();
   const { accessToken } = useAuthStore();
-  const logout = useAuthStore((state) => state.logout);
 
   useEffect(() => {
     if (!accessToken) {
@@ -43,6 +42,10 @@ function Mypage() {
   useEffect(() => {
     fetchUserInfo();
   }, []);
+
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setActiveTab(newValue);
+  };
 
   const handleLogout = () => {
     logout();
@@ -103,46 +106,19 @@ function Mypage() {
   };
 
   return (
-    <Box sx={{ display: "flex", padding: 3, maxWidth: 1200, margin: "0 auto" }}>
-      {/* 왼쪽 메뉴 */}
-      <Box
-        sx={{
-          width: "250px",
-          borderRight: "1px solid #ddd",
-          paddingRight: 2,
-          display: "flex",
-          flexDirection: "column",
-          gap: 2,
-        }}
+    <Box sx={{ padding: 3, maxWidth: 900, margin: "0 auto" }}>
+      <Tabs
+        aria-label="mypage Tabs"
+        value={activeTab}
+        onChange={handleTabChange}
+        sx={{ borderBottom: 1, borderColor: "divider" }}
       >
-        <Button
-          variant="text"
-          color={activeMenu === "profile" ? "primary" : "inherit"}
-          onClick={() => setActiveMenu("profile")}
-          sx={{ textAlign: "left", fontWeight: "bold" }}
-        >
-          내프로필
-        </Button>
-        <Button
-          variant="text"
-          color={activeMenu === "security" ? "primary" : "inherit"}
-          onClick={() => setActiveMenu("security")}
-          sx={{ textAlign: "left", fontWeight: "bold" }}
-        >
-          보안설정
-        </Button>
-        <Button
-          variant="text"
-          onClick={handleLogout}
-          sx={{ textAlign: "left", fontWeight: "bold", marginTop: "auto" }}
-        >
-          로그아웃
-        </Button>
-      </Box>
-
-      {/* 오른쪽 내용 */}
-      <Box sx={{ flex: 1, paddingLeft: 3 }}>
-        {activeMenu === "profile" && userInfo ? (
+        <Tab label="내프로필" value="profile" />
+        <Tab label="보안설정" value="security" />
+        {/* <Tab abel="결제내역" value="orderhistory" /> */}
+      </Tabs>
+      <Box sx={{ paddingTop: 3 }}>
+        {activeTab === "profile" && userInfo ? (
           <Box
             sx={{
               padding: 3,
@@ -183,7 +159,7 @@ function Mypage() {
               </Button>
             </form>
           </Box>
-        ) : activeMenu === "security" ? (
+        ) : activeTab === "security" ? (
           <Box
             sx={{
               padding: 3,
@@ -251,6 +227,19 @@ function Mypage() {
             <Typography variant="h6">결제 내역</Typography>
           </Box>
         )}
+
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleLogout}
+          sx={{
+            fontWeight: "bold",
+            padding: "12px",
+            textTransform: "none",
+          }}
+        >
+          로그아웃
+        </Button>
       </Box>
     </Box>
   );
