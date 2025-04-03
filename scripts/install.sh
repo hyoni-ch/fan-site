31a96c0f728d99e9b397f14f32a3479967ef74e5
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-03 20:45:55 +0900

    env: cicd dependecy add
    s3 cicd 스크립트 추가

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+sudo chown -R ec2-user:ec2-user /joyuri
 cd /joyuri
 
-# 기존 node_modules 삭제 후 새 의존성 설치
+기존 node_modules 삭제 후 새 의존성 설치
 rm -rf node_modules package-lock.json
 npm install
