c507e64b448872c671897b9728c0f356dbd251b8
hyoni-ch <asdfgh9933@daum.net>
2025-04-04 02:33:04 +0900

    굿즈 create

---
+++ src/app/(route)/goods/create/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useEffect } from "react";
+import GoodsCreate from "../_components/goodsCreate";
+import { Box } from "@mui/material";
+import useAuthStore from "@/store/authStore";
+import { useRouter } from "next/navigation";
+
+function Create() {
+  const { roles } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!roles.includes("ROLE_ADMIN")) {
+      console.log("접근할 수 없는 페이지입니다.");
+      router.push("/");
+    }
+  }, [roles, router]);
+
+  return (
+    <Box>
+      <GoodsCreate />
+    </Box>
+  );
+}
+
+export default Create;
