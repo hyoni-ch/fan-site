a40893acb61e052ac4697937a09fbaed98d08190
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-05 19:35:10 +0900

    feat: UserListTable Component UI
    전체 유저 리스트 컴포넌트에서 테이블 컴포넌트 부분 모듈화.
    react-table을 활용한 테이블 인스턴스 생성 및 구조화 설정
    부모 컴포넌트로부터 받은 함수를 호출해서 서버에 업데이트 하는 기능.

--- src/app/(route)/admin/_component/userListTable.tsx
+++ src/app/(route)/admin/_component/userListTable.tsx
@@ -1,147 +1,193 @@
-// UserListTable.tsx
+// components/UserListTable.tsx
 "use client";
-import React, { useMemo, useState } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import {
-  useReactTable,
-  ColumnDef,
-  getCoreRowModel,
-  flexRender,
-} from "@tanstack/react-table";
-import {
+  CircularProgress,
   Table,
   TableBody,
+  TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  TableCell,
   Paper,
-  Typography,
 } from "@mui/material";
-import { SelectChangeEvent } from "@mui/material";
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  ColumnDef,
+} from "@tanstack/react-table";
+import { styled } from "@mui/material/styles";
+
+import { User } from "@/types/iadmin";
 import RoleSelect from "./roleSelect";
 
-interface User {
-  nickname: string;
-  username: string;
-  roles: string[];
-}
+// 테이블 셀 간격 조정을 위한 스타일
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  padding: theme.spacing(1, 3),
+}));
 
 interface UserListTableProps {
   users: User[];
-  onUpdateUserRoles?: (username: string, roles: string[]) => void;
+  allRoles: string[];
+  onSaveRoles: (nickname: string, roles: string[]) => Promise<void>;
+  loading: boolean;
+  error: Error | null;
+  getRoleLabel: (role: string) => string;
 }
 
 const UserListTable: React.FC<UserListTableProps> = ({
   users,
-  onUpdateUserRoles,
+  allRoles,
+  onSaveRoles,
+  loading,
+  error,
+  getRoleLabel,
 }) => {
-  const data = useMemo(() => users, [users]);
-  const [editedRoles, setEditedRoles] = useState<{
-    [username: string]: string[];
+  const [selectedRolesInternal, setSelectedRolesInternal] = useState<{
+    [nickname: string]: string[];
   }>({});
 
-  const handleRoleChange =
-    (username: string) => (event: SelectChangeEvent<string[]>) => {
-      const selectedRoles = event.target.value as string[];
-      setEditedRoles((prev) => ({ ...prev, [username]: selectedRoles }));
-    };
+  // 역할 드롭다운 값 변경 시 호출 콜백함수.
+  const handleRoleChangeInternal = useCallback(
+    (nickname: string, newRolesWithoutPrefix: string[]) => {
+      // 접두사에 ROLE_ 붙여서 저장.
+      const newRolesWithPrefix = newRolesWithoutPrefix.map(
+        (role) => `ROLE_${role}`
+      );
+      // 임시 선택 역할 상태 업데이트
+      setSelectedRolesInternal((prev) => ({
+        ...prev,
+        [nickname]: newRolesWithPrefix,
+      }));
+    },
+    [setSelectedRolesInternal]
+  );
 
-  const handleApplyChanges = (username: string) => () => {
-    if (onUpdateUserRoles) {
-      onUpdateUserRoles(username, editedRoles[username] || []);
-    }
-  };
+  // 저장 버튼 클릭 시 호출 콜백 함수
+  const handleSaveRolesInternal = useCallback(
+    async (nickname: string) => {
+      // 임시 선택 역할 상태에 있는 사용자의 역할 목록 업데인트
+      const rolesToUpdate = selectedRolesInternal[nickname] || [];
+      // 저장 완료 시 선택 역할 상태에서 해당 사용자 정보 제거
+      await onSaveRoles(nickname, rolesToUpdate);
+      setSelectedRolesInternal((prev) => {
+        const newState = { ...prev };
+        delete newState[nickname];
+        return newState;
+      });
+    },
+    [selectedRolesInternal, onSaveRoles]
+  );
 
+  // tanstack table 컬럼 정의
   const columns = useMemo<ColumnDef<User>[]>(
     () => [
       {
-        header: "닉네임",
+        header: "닉네임", // 테이블 헤더 텍스트
         accessorKey: "nickname",
-        cell: ({ getValue }) => (
-          <Typography variant="body2" fontWeight="bold">
-            {getValue() as string}
-          </Typography>
-        ),
       },
       {
-        header: "유저명",
+        header: "이메일",
         accessorKey: "username",
-        cell: ({ getValue }) => (
-          <Typography variant="body2" color="text.secondary">
-            {getValue() as string}
-          </Typography>
-        ),
       },
       {
-        header: "권한",
-        accessorKey: "roles",
+        header: "현재 역할",
+        // user 객체에서 roles배열 가져온 후 UI 레이블로 변환 하고 쉼표로 연결하기.
+        accessorFn: (row) => row.roles?.map(getRoleLabel).join(", ") || "없음",
+        id: "currentRoles",
+      },
+      {
+        header: "역할 변경",
+        id: "editRoles",
+        // 각 셀 내용 렌더링 부분.
         cell: ({ row }) => {
-          const username = row.original.username;
-          const currentRoles =
-            editedRoles[username] || row.original.roles || [];
+          const nickname = row.original.nickname;
+          // 현재 유저가 임시 선택한 역할 목록 or 현재 역할 목록
+          const currentUserSelectedRolesWithPrefix =
+            selectedRolesInternal[nickname] || row.original.roles || [];
+          // ROLE_ 제거하고 UI에 표시할 역할 목록 생성.
+          const currentUserSelectedRolesWithoutPrefix =
+            currentUserSelectedRolesWithPrefix.map((role) =>
+              getRoleLabel(role)
+            );
+
           return (
+            // 역할 선택 UI 컴포넌트.
             <RoleSelect
-              username={username}
-              currentRoles={currentRoles}
-              handleRoleChange={handleRoleChange}
-              handleApplyChanges={handleApplyChanges}
+              nickname={nickname}
+              allRoles={allRoles}
+              selectedRoles={currentUserSelectedRolesWithoutPrefix}
+              getRoleLabel={getRoleLabel}
+              onChange={handleRoleChangeInternal}
+              onSave={handleSaveRolesInternal}
             />
           );
         },
       },
     ],
-    [editedRoles]
+    [
+      allRoles,
+      selectedRolesInternal,
+      handleRoleChangeInternal,
+      handleSaveRolesInternal,
+      getRoleLabel,
+    ]
   );
 
+  // Table 인스턴스 생성
   const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
+    data: users, // 테이블 데이터 (부모 컴포에서 props로 전달)
+    columns, // 테이블 컬럼 정의
+    getCoreRowModel: getCoreRowModel(), // 기본 행 모델 생성 함수
   });
 
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <TableContainer
-      component={Paper}
-      sx={{ mt: 2, p: 2, borderRadius: 2, boxShadow: 2 }}
-    >
-      <Table>
-        <TableHead sx={{ backgroundColor: "#f5f5f5" }}>
+    <TableContainer component={Paper}>
+      <Table aria-label="user list table">
+        <TableHead>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <TableCell
-                  key={header.id}
-                  sx={{ fontWeight: "bold", textAlign: "center" }}
-                >
+                <StyledTableCell key={header.id}>
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext()
                   )}
-                </TableCell>
+                </StyledTableCell>
               ))}
             </TableRow>
           ))}
         </TableHead>
         <TableBody>
-          {table.getRowModel().rows.length > 0 ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id} hover>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id} sx={{ textAlign: "center" }}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={3} sx={{ textAlign: "center", py: 3 }}>
-                <Typography variant="body2" color="text.secondary">
-                  유저 데이터가 없습니다.
-                </Typography>
-              </TableCell>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <StyledTableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </StyledTableCell>
+              ))}
             </TableRow>
-          )}
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
