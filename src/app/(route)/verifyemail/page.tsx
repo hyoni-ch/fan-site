c59a626e57cc647b051b603c08e6297a9db77fcc
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-08 14:26:22 +0900

    feat: 이메일 인증 후 처리 (에러핸들링)
    이메일 인증 후 성공 및 에러 핸들링
    mui 스낵바 사용해서 UI 표시

--- src/app/(route)/verifyemail/page.tsx
+++ src/app/(route)/verifyemail/page.tsx
@@ -1,9 +1,11 @@
+"use client";
 import React, { Suspense } from "react";
 import VerifyEmailServer from "./_components/verifyEmailServer";
+import LoadingIndicator from "@/components/LoadingIndicator";
 
 function VerifyEmail() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<LoadingIndicator />}>
       <VerifyEmailServer />
     </Suspense>
   );
