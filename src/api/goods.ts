d81d8495ea59819e4ddaad606b886f70e72c7795
hyoni-ch <asdfgh9933@daum.net>
2025-04-10 20:56:42 +0900

    굿즈 업데이트 api 이미지 관련 수정

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,5 +1,39 @@
 import api from "@/utils/api";
 
+// 굿즈 CREATE
+export const createGoods = async (
+  name: string,
+  description: string,
+  price: string,
+  image: File
+) => {
+  if (!name || !description || !price || !image) {
+    throw new Error("모든 것을 채워주세요!");
+    return false;
+  }
+
+  try {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("price", price);
+    formData.append("image", image);
+
+    const result = await api.post("/goods/add", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(result);
+
+    return true;
+  } catch (error) {
+    console.error("굿즈 업로드 실패", error);
+    throw error;
+  }
+};
+
+// 굿즈 리스트 READ
 export const getGoodsList = async (params: {
   sort: string;
   name: string;
@@ -15,6 +49,7 @@ export const getGoodsList = async (params: {
   }
 };
 
+// 굿즈 디테일 READ
 export const getGoodsDetail = async (id: number) => {
   try {
     const response = await api.get(`/goods/${id}`);
@@ -25,33 +60,48 @@ export const getGoodsDetail = async (id: number) => {
   }
 };
 
-export const createGoods = async (
+// 굿즈 수정 UPDATE
+export const updateGoods = async (
+  id: number,
   name: string,
   description: string,
   price: string,
-  image: File
+  image: File | null
 ) => {
-  const formData = new FormData();
-  formData.append("name", name);
-  formData.append("description", description);
-  formData.append("price", price);
-  formData.append("image", image);
-  if (!name || !description || !price || !image) {
+  if (!name || !description || !price) {
     throw new Error("모든 것을 채워주세요!");
     return false;
   }
-
   try {
-    const result = await api.post("/goods/add", formData, {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("price", price);
+    if (image) {
+      formData.append("image", image);
+    }
+
+    const result = await api.post(`/goods/${id}/update`, formData, {
       headers: {
-        "Content-Type": "multipart/form-data", // multipart/form-data를 명시적으로 지정
+        "Content-Type": "multipart/form-data",
       },
     });
     console.log(result);
 
     return true;
   } catch (error) {
-    console.error("굿즈 업로드 실패", error);
+    console.error("굿즈 업데이트 Error:", error);
+    throw error;
+  }
+};
+
+// 굿즈 DELETE
+export const deleteGoods = async (id: number) => {
+  try {
+    const response = await api.delete(`/goods/${id}/delete`);
+    return response.data;
+  } catch (error) {
+    console.error("굿즈 삭제 Error:", error);
     throw error;
   }
 };
