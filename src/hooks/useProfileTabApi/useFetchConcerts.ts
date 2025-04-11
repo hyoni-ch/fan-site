0c658526d732e7c787ae30f65bfb44765e7164c9
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-11 23:45:45 +0900

    refac: fetch api 함수 hook으로 분리
    1. 앨범 리스트 가져오기 함수
    2. 드라마 리스트 가져오기 함수
    3. 콘서트 리스트 가져오기 함수
    hook 으로 분리해서 사용함으로써 유지보수성 높이고
    추후 이 훅을 재사용 가능성도 고려
    및 UI 컴포넌트에서 분리함으로써 조금 더 깔끔한 컴포넌트 구성 기대.

---
+++ src/hooks/useProfileTabApi/useFetchConcerts.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { getConcertList } from "@/api/profile";
+import { ConcertList } from "@/types/iprofile";
+
+function useFetchConcerts() {
+  const [concertsData, setConcertsData] = useState<ConcertList[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchConcerts = async () => {
+      try {
+        const concertList = await getConcertList();
+        setConcertsData(concertList);
+        setLoading(false);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error);
+        } else {
+          setError(new Error("Unknown error occurred"));
+        }
+        setLoading(false);
+      }
+    };
+
+    fetchConcerts();
+  }, []);
+
+  return { concertsData, loading, error };
+}
+
+export default useFetchConcerts;
