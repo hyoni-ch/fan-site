7e27f4e59580358f178adfccf9a80caf570a6d35
hyoni-ch <asdfgh9933@daum.net>
2025-04-12 01:25:09 +0900

    서브 레이아웃 메인 페이지 경로 추가

--- src/app/subLayout.tsx
+++ src/app/subLayout.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 
 export default function SubLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-  const noMarginPaths = ["/artist"]; // 제외 경로 추가~
+  const noMarginPaths = ["/", "/artist"]; // 제외 경로 추가~
 
   const applyMarginTop = !noMarginPaths.includes(pathname);
 
