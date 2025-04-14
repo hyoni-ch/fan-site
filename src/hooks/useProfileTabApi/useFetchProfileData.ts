0de7bfefa7a8545140f414165f4cdd547e989a91
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-14 23:01:20 +0900

    feat: minLoadingTime 직접 설정 방식
    1. 이 방식은 데이터 요청이 성공하든 실패하든, 설정된 시간 만큼의 로딩 상태를 표시
    -> 보통 일관된 사용자 UX를 제공한다거나, 시각적 피드백의 관점에 장점은 있지만
    -> 불필요한 지연이나 성능 오버헤드를 일으킬 가능성이 있음
    +++
    추후에 promise.race()를 사용한 방식으로 리팩토링하고 성능 개선을 할 때, 얼마나 퍼포먼스가 증가할 수 있는지 예측하고, 직접 측정해보기.

--- src/hooks/useProfileTabApi/useFetchProfileData.ts
+++ src/hooks/useProfileTabApi/useFetchProfileData.ts
@@ -1,17 +1,46 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 
-function useFetchProfileData<T>(fetchFunction: () => Promise<T>) {
+function useFetchProfileData<T>(
+  fetchFunction: () => Promise<T>,
+  // 최소 로딩 시간 (ms)
+  minLoadingTime: number = 0
+) {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  // 타이머 직접 설정
+  const loadingTimer = useRef<NodeJS.Timeout | null>(null);
+  const hasFetched = useRef(false); // 첫 데이터 로딩 완료 여부 추적
 
   const fetchData = useCallback(async () => {
     setLoading(true);
     setError(null);
 
+    // 시작 시간 받기
+    const startTime = Date.now();
+
     try {
       const response = await fetchFunction();
       setData(response);
+      // 데이터 로딩 성공
+      hasFetched.current = true;
+
+      const endTime = Date.now();
+      const elapsedTime = endTime - startTime;
+
+      if (elapsedTime < minLoadingTime) {
+        //최소 로딩 시간보다 짧게 걸리면, 남은 시간만큼 딜레이 후 로딩 상태 해제
+        await new Promise(
+          (resolve) =>
+            (loadingTimer.current = setTimeout(
+              resolve,
+              minLoadingTime - elapsedTime
+            ))
+        );
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
     } catch (err: unknown) {
       if (err instanceof Error) {
         setError(err);
@@ -19,9 +48,14 @@ function useFetchProfileData<T>(fetchFunction: () => Promise<T>) {
         setError(new Error("Unknown error occurred"));
       }
     } finally {
-      setLoading(false);
+      if (loadingTimer.current) {
+        clearTimeout(loadingTimer.current);
+      }
+      if (!hasFetched.current) {
+        setLoading(false);
+      }
     }
-  }, [fetchFunction]);
+  }, [fetchFunction, minLoadingTime]);
 
   useEffect(() => {
     fetchData();
