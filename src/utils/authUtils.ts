84efd36137bd5334129fd3ae746cf509bd902d94
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-23 18:36:00 +0900

    fix: Store에서 함수 Util로 분리해서 사용
    이 부분은 추후 직접 접근하는 방식으로 수정해서 제거해야할 부분

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,7 +1,7 @@
 // 레거시 코드 부분
-// import useAuthStore from "@/store/authStore";
+import useAuthStore from "@/store/authStore";
 
 // export const getAccessToken = () => useAuthStore.getState().accessToken;
 // export const getUserName = () => useAuthStore.getState().username;
 // export const getUserNickname = () => useAuthStore.getState().userNickname;
-// export const getUserRoles = () => useAuthStore.getState().roles;
+export const getUserRoles = () => useAuthStore.getState().roles;
