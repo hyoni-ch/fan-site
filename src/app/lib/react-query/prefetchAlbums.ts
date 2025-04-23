39582b14f9c245de880e21df3e19a24b000e3a94
zoeyourlife <201814112@office.skhu.ac.kr>
2025-04-23 18:37:18 +0900

    refac: 이전에 앨범 탭 한정 커스텀 훅 제거
    1. 이전에 앨범 탭 한정으로 react-query 캐시 적용 및  zustand를 활용한 전역 상태 저장 및 prefetch 기능 다른 탭에도 확장하면서 사용하지 않게 된 레거시 코드

--- src/app/lib/react-query/prefetchAlbums.ts
+++ src/app/lib/react-query/prefetchAlbums.ts
@@ -1,23 +1,24 @@
-import { QueryClient } from "@tanstack/react-query";
-import { getAlbumList } from "@/api/discography";
-import { useAlbumStore } from "@/store/albumStore";
+//TODO 기존에 앨범에만 적용한 react-query 캐시 전략 및 zustand 연동한 전역 preload 기능 legacy 코드
+// import { QueryClient } from "@tanstack/react-query";
+// import { getAlbumList } from "@/api/discography";
+// import { useAlbumStore } from "@/store/albumStore";
 
-export const prefetchAlbums = async (queryClient: QueryClient) => {
-  try {
-    const data = await queryClient.fetchQuery({
-      queryKey: ["profile", "albums"],
-      queryFn: getAlbumList,
-      staleTime: 1000 * 60 * 5, // 5분 캐시
-    }); // zustand에도 저장
+// export const prefetchAlbums = async (queryClient: QueryClient) => {
+//   try {
+//     const data = await queryClient.fetchQuery({
+//       queryKey: ["profile", "albums"],
+//       queryFn: getAlbumList,
+//       staleTime: 1000 * 60 * 5, // 5분 캐시
+//     }); // zustand에도 저장
 
-    console.log("✅ prefetch된 앨범 데이터:", data);
-    useAlbumStore.getState().setAlbums(data);
+//     console.log("✅ prefetch된 앨범 데이터:", data);
+//     useAlbumStore.getState().setAlbums(data);
 
-    const zustandAlbums = useAlbumStore.getState().albums;
-    console.log("✅ zustand에 저장된 앨범 데이터:", zustandAlbums);
+//     const zustandAlbums = useAlbumStore.getState().albums;
+//     console.log("✅ zustand에 저장된 앨범 데이터:", zustandAlbums);
 
-    return data;
-  } catch (error) {
-    console.error("preload 실패:", error);
-  }
-};
+//     return data;
+//   } catch (error) {
+//     console.error("preload 실패:", error);
+//   }
+// };
